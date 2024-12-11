6c6c37e6c7d00546e47750d6141f1da651433983
Raizak-17 <victorantihuala12@gmail.com>
2024-12-11 05:53:59 -0300

    jjhhj

--- TallerMecanico/urls.py
+++ TallerMecanico/urls.py
@@ -45,5 +45,5 @@ urlpatterns = [
     path('trabajos/<int:trabajo_id>/detalles/', detalles_reparaciones, name='trabajos_detalles_reparaciones'),
     path('reset_password/<str:rut>/', reset_password, name='reset_password'),
     path('solicitar_cita/', solicitar_cita, name='solicitar_cita'),
-    path('estado_vehiculo/', estado_vehiculo, name='estado_vehiculo')
+    path('estado_vehiculos/', estado_vehiculo, name='estado_vehiculos')
 ]
